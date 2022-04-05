49e78494e9b60c31ddbea0133489cb82877818b6
jaehyun <>
2022-04-05 10:56:12 +0900

    Update section.vue

--- src/components/section.vue
+++ src/components/section.vue
@@ -1,20 +1,14 @@
 <template>
     <div>
-        <!-- <a href="/order">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil reiciendis facere quo eum quisquam ad molestias dignissimos quidem quia odio laboriosam explicabo impedit, quod corrupti minima tempore quasi distinctio soluta eaque mollitia assumenda numquam voluptatem iusto quis! Impedit, consequuntur. Adipisci est perferendis quo inventore fugiat totam quos autem culpa! Adipisci.</a> -->
         <router-view></router-view>
     </div>
 </template>
 
 <script>
-// import main_ from '../pages/main.vue'
-export default {
-    name: 'section_',
-    components: {
-        // main_,
-    },
-}
+    export default {
+        name: 'section_',
+        components: {
+            // main_,
+        },
+    }
 </script>
-
-<style>
-
-</style>
