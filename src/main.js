2a914eba5b30695d4f8bceb87d9bf6d0d9e43ab9
user name <wogusdl123@gmail.com>
2022-05-09 16:03:58 +0900

    🚀

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router';
 import { mainPage, orderPage, confirmPage, directionsPage } from './components/pages'
+import './registerServiceWorker'
 
 export const eventBus = new Vue()
 
